435157bb1617c8f1efeaaff25944bc6170907d08
psnaz <psnazle@gmail.com>
2022-03-10 12:51:34 +0000

    Add code to enable shuffling questions and to display question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,39 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion() {
 
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { tetxt: '4', correct: true },
+            { tetxt: '22', correct: false },
+        ]
+    }
+]
