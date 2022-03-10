8e688085b5ecbc9c600be551de14fa979c9614ba
psnaz <psnazle@gmail.com>
2022-03-10 13:13:24 +0000

    Determine the correct answer and create eventlistener for the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,10 @@ function showQuestion(question) {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
-
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener ('click', selectAnswer)
     })
 }
 
