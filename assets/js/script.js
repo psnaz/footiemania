d39ee6cf69f821655c15df74501d4894cd7f0690
psnaz <psnazle@gmail.com>
2022-03-10 12:24:38 +0000

    Add event listeners for start button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-function startGame() {
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -7,5 +15,5 @@ function setNextQuestion() {
 }
 
 function selectAnswer() {
-    
+
 }
