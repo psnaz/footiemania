7615542bc8a725c5f8b445ef3abd2dfc1a7d2040
psnaz <psnazle@gmail.com>
2022-05-09 21:10:51 +0000

    Fix bug: restore background and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,30 @@
  *  Code based on Youtube tutorial 'Build a Quiz App with JavaScript'by WebDev Simplified - coded along, 
  * please see the link in Readme file 
  * */
-
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
+const gameArea = document.querySelector("#game-area");
 
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-    //setBackground(document.querySelector("#game-area"), neutral); RESET TO BLUE!!!!
+    restoreBackground();
     currentQuestionIndex++;
-    setNextQuestion();    
+    setNextQuestion();
 });
-
 
 
 /**
  * Starts the game when start button's clicked, questions are shuffled and a question gets set
  */
-
 function startGame() {
-    document.getElementById("score").innerText = 0;       // My code        
-    document.getElementById("incorrect").innerText = 0;   // My code
-    console.log('Started');                               // My code
+    document.getElementById("score").innerText = 0; // My code        
+    document.getElementById("incorrect").innerText = 0; // My code
+    console.log('Started'); // My code
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
@@ -58,7 +56,7 @@ function showQuestion(question) {
         if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener ('click', selectAnswer);
+        button.addEventListener('click', selectAnswer);
         answerButtonsElement.appendChild(button);
     });
 }
@@ -83,24 +81,32 @@ function resetState() {
 function selectAnswer(e) {
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct;
-    const button = Array.from(document.getElementsByClassName('btn')) // My code
-    button.forEach(btn => {                                           // My code
-        btn.removeEventListener('click', selectAnswer)                // My code
-    })                                                                // My code
-    setStatusClass(document.querySelector("#score"), correct);        
-    if (correct) {                                                    // My code
-        incrementScore();                                             // My code
-    } else {                                                          // My code
-        incrementWrongAnswer();                                       // My code
-    }                                                           
-    setStatusClass(document.querySelector("#game-area"), correct);           
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct);
+    const buttons = Array.from(document.getElementsByClassName('btn')) // My code
+
+    // Set the color to the selected answer
+    setStatusClass(selectedButton, correct);
+
+    buttons.forEach((btn) => { // My code
+        btn.removeEventListener('click', selectAnswer);
+        // We turn green the correct answer when the select was wrong
+        if (!correct && btn.dataset.correct) {
+            setStatusClass(btn, btn.dataset.correct);
+        }
     });
-    if (shuffledQuestions.length > currentQuestionIndex +1) {
+
+    if (correct) { // My code
+        incrementScore(); // My code
+    } else { // My code
+        incrementWrongAnswer(); // My code
+    }
+
+    // Set background color based on the answer
+    setStatusClass(gameArea, correct);
+
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide');
     } else {
-        startButton.innerText = "Let's play again!";            // My own content
+        startButton.innerText = "Let's play again!"; // My own content
         startButton.classList.remove('hide');
     }
 }
@@ -127,17 +133,17 @@ function setStatusClass(element, correct) {
 function clearStatusClass(element) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
-} 
+    element.classList.remove('neutral');
+}
 
 
 /**
  * Resets the background behind the game area 
  * back to blue - neutral
  */
-
-function setBackground(element, neutral) {
-    element.classList.add('neutral');
-}   
+function restoreBackground() {
+    gameArea.classList.add('neutral');
+}
 
 
 /**
@@ -145,10 +151,8 @@ function setBackground(element, neutral) {
  * - code from CI Love Maths Walkthrough Project
  */
 function incrementScore() {
-
     let oldscore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldscore;
-
 }
 
 /**
@@ -156,116 +160,233 @@ function incrementScore() {
  *  - code from CI Love Maths Walkthrough Project
  */
 
- function incrementWrongAnswer() {
-
+function incrementWrongAnswer() {
     let oldscore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldscore;
-
 }
 
 /**
  * My own content for the questions but code taken and modified 
  * from 'Build a Quiz App with JavaScript'by WebDev Simplified 
- */ 
-
-const questions = [
-    {
+ */
+const questions = [{
         question: 'He is one of the best football players in history, he is Argentinian and ruined his career because of drugs. Who is he?',
-        answers: [
-            { text: 'Diego Maradona', correct: true },
-            { text: 'Pelé', correct: false },
-            { text: 'Johann Criff', correct: false },
-            { text: 'Adrian Mutu', correct: false },
+        answers: [{
+                text: 'Diego Maradona',
+                correct: true
+            },
+            {
+                text: 'Pelé',
+                correct: false
+            },
+            {
+                text: 'Johann Criff',
+                correct: false
+            },
+            {
+                text: 'Adrian Mutu',
+                correct: false
+            },
         ]
     },
     {
         question: 'He is English, played with Frank Lampard for the National Team and is one of the best Liverpool players ever. Who is he?',
-        answers: [
-            { text: 'Wayne Rooney', correct: false },
-            { text: 'Steven Gerrard', correct: true },
-            { text: 'Joe Hard', correct: false },
-            { text: 'Glen Johnson', correct: false },
+        answers: [{
+                text: 'Wayne Rooney',
+                correct: false
+            },
+            {
+                text: 'Steven Gerrard',
+                correct: true
+            },
+            {
+                text: 'Joe Hard',
+                correct: false
+            },
+            {
+                text: 'Glen Johnson',
+                correct: false
+            },
         ]
     },
     {
         question: 'He is from Belgium, was one of the most amazing players for Chelsea, but he has not played that well for Real Madrid. Who is he?',
-        answers: [
-            { text: 'Romelu Lukaku', correct: false },
-            { text: 'Thorgan Hazard', correct: false },
-            { text: 'Jan Vertronghen', correct: false },
-            { text: 'Eden Hazard', correct: true },
+        answers: [{
+                text: 'Romelu Lukaku',
+                correct: false
+            },
+            {
+                text: 'Thorgan Hazard',
+                correct: false
+            },
+            {
+                text: 'Jan Vertronghen',
+                correct: false
+            },
+            {
+                text: 'Eden Hazard',
+                correct: true
+            },
         ]
     },
     {
         question: 'He is from France, he is young and has very heavy links to Real Madrid. Who is he?',
-        answers: [
-            { text: 'Ousmane Dembélé', correct: false },
-            { text: 'Kylian Mbappé', correct: true },
-            { text: 'Kingsley Coman', correct: false },
-            { text: 'Anthony Martial ', correct: false },
+        answers: [{
+                text: 'Ousmane Dembélé',
+                correct: false
+            },
+            {
+                text: 'Kylian Mbappé',
+                correct: true
+            },
+            {
+                text: 'Kingsley Coman',
+                correct: false
+            },
+            {
+                text: 'Anthony Martial ',
+                correct: false
+            },
         ]
     },
     {
         question: 'He is Italian, came through the Academy of AC Milan, he never left the club and retired there. Who is he?',
-        answers: [
-            { text: 'Gianni Rivera', correct: false },
-            { text: 'Roberto Baggio', correct: false },
-            { text: 'Gianluigi Buffon', correct: false },
-            { text: 'Paolo Maldini', correct: true },
+        answers: [{
+                text: 'Gianni Rivera',
+                correct: false
+            },
+            {
+                text: 'Roberto Baggio',
+                correct: false
+            },
+            {
+                text: 'Gianluigi Buffon',
+                correct: false
+            },
+            {
+                text: 'Paolo Maldini',
+                correct: true
+            },
         ]
     },
     {
         question: 'He is a famous striker, a Liverpool legend, but comes from Wales. Who is he?',
-        answers: [
-            { text: 'Aaron Ramsey', correct: false },
-            { text: 'Gareth Bale', correct: false },
-            { text: 'Ian Rush', correct: true },
-            { text: 'Ryan Giggs', correct: false },
+        answers: [{
+                text: 'Aaron Ramsey',
+                correct: false
+            },
+            {
+                text: 'Gareth Bale',
+                correct: false
+            },
+            {
+                text: 'Ian Rush',
+                correct: true
+            },
+            {
+                text: 'Ryan Giggs',
+                correct: false
+            },
         ]
     },
     {
         question: 'Manchester City recently signed him for 100 million pounds, he is English, but they do not like his attitude. Who is he?',
-        answers: [
-            { text: 'Jack Grealish', correct: true },
-            { text: 'Phil Foden', correct: false },
-            { text: 'Kyle Walker', correct: false },
-            { text: 'John Stones', correct: false },
+        answers: [{
+                text: 'Jack Grealish',
+                correct: true
+            },
+            {
+                text: 'Phil Foden',
+                correct: false
+            },
+            {
+                text: 'Kyle Walker',
+                correct: false
+            },
+            {
+                text: 'John Stones',
+                correct: false
+            },
         ]
     },
     {
         question: 'He has played for Liverpool FC, FC Barcelona and Atletico Madrid, and he is "famous" for biting other footballers. Who is he?',
-        answers: [
-            { text: 'Christiano Ronaldo', correct: false },
-            { text: 'Luis Suárez', correct: true },
-            { text: 'Edison Cavani', correct: false },
-            { text: 'Alessandro Nesta', correct: false },
+        answers: [{
+                text: 'Christiano Ronaldo',
+                correct: false
+            },
+            {
+                text: 'Luis Suárez',
+                correct: true
+            },
+            {
+                text: 'Edison Cavani',
+                correct: false
+            },
+            {
+                text: 'Alessandro Nesta',
+                correct: false
+            },
         ]
     },
     {
         question: 'He currently plays for AC Milan, but he has been to many clubs and he is 40 years old. Who is he?',
-        answers: [
-            { text: 'Thomas Müller', correct: false },
-            { text: 'Zlatan Ibrahimović', correct: true },
-            { text: 'Edin Džeko', correct: false },
-            { text: 'Sergio Ramos', correct: false },
+        answers: [{
+                text: 'Thomas Müller',
+                correct: false
+            },
+            {
+                text: 'Zlatan Ibrahimović',
+                correct: true
+            },
+            {
+                text: 'Edin Džeko',
+                correct: false
+            },
+            {
+                text: 'Sergio Ramos',
+                correct: false
+            },
         ]
     },
     {
         question: 'He is Croatian, he scored in the World Cup final 2018 and he has always played as a forward. Who is he?',
-        answers: [
-            { text: 'Mario Mandžukić', correct: true },
-            { text: 'Luka Modrić', correct: false },
-            { text: 'Ivan Rakitić', correct: false },
-            { text: 'Alen Bokšić', correct: false },
+        answers: [{
+                text: 'Mario Mandžukić',
+                correct: true
+            },
+            {
+                text: 'Luka Modrić',
+                correct: false
+            },
+            {
+                text: 'Ivan Rakitić',
+                correct: false
+            },
+            {
+                text: 'Alen Bokšić',
+                correct: false
+            },
         ]
     },
     {
         question: 'He plays as a left winger for Chelsea, they signed him from Borussia Dortmund, but he is from the USA. Who is he?',
-        answers: [
-            { text: 'Giovanni Reyna', correct: false },
-            { text: 'Christian Pulisic', correct: true },
-            { text: 'Sergiño Dest', correct: false },
-            { text: 'Weston McKennie', correct: false },
+        answers: [{
+                text: 'Giovanni Reyna',
+                correct: false
+            },
+            {
+                text: 'Christian Pulisic',
+                correct: true
+            },
+            {
+                text: 'Sergiño Dest',
+                correct: false
+            },
+            {
+                text: 'Weston McKennie',
+                correct: false
+            },
         ]
     },
 ];
